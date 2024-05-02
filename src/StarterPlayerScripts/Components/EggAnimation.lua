18234dbb6f450661099aab3d092318731c44b56f
Curse <37249669+Curscs@users.noreply.github.com>
2024-05-02 20:28:23 +0100

    update 1.2

--- src/StarterPlayerScripts/Components/EggAnimation.lua
+++ src/StarterPlayerScripts/Components/EggAnimation.lua
@@ -15,9 +15,9 @@ function EggAnimation:Construct()
 end
 
 EggAnimation.Started:Connect(function(component)
-    local Egg = component.Instance
-    if Egg:GetAttribute("Type") == "Egg" then
-            local totaleggs = 0
+    local RobloxInstance = component.Instance
+    if RobloxInstance:GetAttribute("Type") == "Egg" then
+        local totaleggs = 0
         for _, _ in pairs(Workspace.Animations.EggHatching:GetChildren()) do
             totaleggs += 1
         end
@@ -25,48 +25,48 @@ EggAnimation.Started:Connect(function(component)
         local EggCFrameVal = Instance.new("CFrameValue")
         Connections["EggRuntime"] = game:GetService("RunService").RenderStepped:Connect(function()
             if totaleggs == 1 then
-                Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(0,0,0) * EggCFrameVal.Value)
+                RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(0,0,0) * EggCFrameVal.Value)
             elseif totaleggs == 2 then
-                if Egg.Name == "1" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,0,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "2" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,0,0) * EggCFrameVal.Value)
+                if RobloxInstance.Name == "1" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "2" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,0,0) * EggCFrameVal.Value)
                 end
             elseif totaleggs == 3 then
-                if Egg.Name == "1" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-4,0,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "2" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(0,0,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "3" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(4,0,0) * EggCFrameVal.Value)
+                if RobloxInstance.Name == "1" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-4,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "2" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(0,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "3" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(4,0,0) * EggCFrameVal.Value)
                 end
             elseif totaleggs == 4 then
-                if Egg.Name == "1" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,0,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "2" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,0,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "3" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,0,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "4" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,0,0) * EggCFrameVal.Value)
+                if RobloxInstance.Name == "1" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "2" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "3" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "4" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,0,0) * EggCFrameVal.Value)
                 end
             elseif totaleggs == 8 then
-                if Egg.Name == "1" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,-2.5,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "2" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,-2.5,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "3" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,-2.5,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "4" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,-2.5,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "5" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,2.5,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "6" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,2.5,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "7" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,2.5,0) * EggCFrameVal.Value)
-                elseif Egg.Name == "8" then
-                    Egg:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,2.5,0) * EggCFrameVal.Value)
+                if RobloxInstance.Name == "1" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,-2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "2" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,-2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "3" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,-2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "4" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,-2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "5" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "6" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "7" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "8" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,2.5,0) * EggCFrameVal.Value)
                 end
             end
         end)
@@ -82,8 +82,76 @@ EggAnimation.Started:Connect(function(component)
             Right:Play()Right.Completed:Wait()
         end
         Bounce:Destroy()Left:Destroy()Right:Destroy()
-        Egg:RemoveTag(EggAnimation.Tag)
-        Egg:Destroy()
+        RobloxInstance:RemoveTag(EggAnimation.Tag)
+        RobloxInstance:Destroy()
+    elseif RobloxInstance:GetAttribute("Type") == "Pet" then
+        local totaleggs = 0
+        for _, _ in pairs(Workspace.Animations.EggHatching:GetChildren()) do
+            totaleggs += 1
+        end
+        -- { Egg Init } --
+        local EggCFrameVal = Instance.new("CFrameValue")
+        Connections["EggRuntime"] = game:GetService("RunService").RenderStepped:Connect(function()
+            if totaleggs == 1 then
+                RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(0,0,0) * EggCFrameVal.Value)
+            elseif totaleggs == 2 then
+                if RobloxInstance.Name == "1" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "2" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,0,0) * EggCFrameVal.Value)
+                end
+            elseif totaleggs == 3 then
+                if RobloxInstance.Name == "1" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-4,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "2" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(0,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "3" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(4,0,0) * EggCFrameVal.Value)
+                end
+            elseif totaleggs == 4 then
+                if RobloxInstance.Name == "1" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "2" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "3" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,0,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "4" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,0,0) * EggCFrameVal.Value)
+                end
+            elseif totaleggs == 8 then
+                if RobloxInstance.Name == "1" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,-2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "2" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,-2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "3" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,-2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "4" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,-2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "5" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-6,2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "6" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(-2,2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "7" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(6,2.5,0) * EggCFrameVal.Value)
+                elseif RobloxInstance.Name == "8" then
+                    RobloxInstance:PivotTo(Camera.CFrame * EggOffset * CFrame.new(2,2.5,0) * EggCFrameVal.Value)
+                end
+            end
+        end)
+        EggCFrameVal.Value = CFrame.new(0,8,0)
+        -- { Egg Tweens } --
+        local Bounce = game:GetService("TweenService"):Create(EggCFrameVal, TweenInfo.new(1, Enum.EasingStyle.Bounce), {Value = CFrame.new()})
+        local Left = game:GetService("TweenService"):Create(EggCFrameVal, TweenInfo.new(0.1, Enum.EasingStyle.Sine), {Value = CFrame.Angles(0,0,math.rad(25))})
+        local Right = game:GetService("TweenService"):Create(EggCFrameVal, TweenInfo.new(0.1, Enum.EasingStyle.Sine), {Value = CFrame.Angles(0,0,math.rad(-25))})
+        -- { Egg Animation } --
+        Bounce:Play()Bounce.Completed:Wait()
+        for i = 1, 10 do
+            Left:Play()Left.Completed:Wait()
+            Right:Play()Right.Completed:Wait()
+        end
+        Bounce:Destroy()Left:Destroy()Right:Destroy()
+        RobloxInstance:RemoveTag(EggAnimation.Tag)
+        RobloxInstance:Destroy()
     end
     -- { Pet Init } --
 
